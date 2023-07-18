f75cb38901db446f130f92e08d37bb6b26e3d40d
BerdinaKatya <kuznkate42@gmail.com>
2023-07-18 17:41:59 +0500

    Initial Commit

---
+++ Task 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 1
+//
+//  Created by Екатерина Кузнецова on 18.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
